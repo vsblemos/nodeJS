c127a91c60c6af1b0d2dc47c622faa4199149c29
Victor Serra B Lemos <91221341+vsblemos@users.noreply.github.com>
2022-08-01 15:59:23 -0300

    create post route

--- routes.js
+++ routes.js
@@ -2,11 +2,23 @@ const Router = require("express");
 //const transactionRoutes = require("./src/modules/Routes/transactionRoutes")
 const routes = Router();
 
+const users = []
 
 routes.get("/users", (request,response)=>{
     response.json({ message: "user route" });
 });
 
+routes.post("/users", (request,response)=>{
+    const name = request.body.name;
+    const username = request.body.username;
+    const email = request.body.email;
+
+    const user = {name, username,email}
+
+    users.push(user);
+
+    return responde.json({user});
+});
 
 
 
