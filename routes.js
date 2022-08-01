58e66302690e2e4e4517066a4d98a00bf94befa9
Victor Serra B Lemos <91221341+vsblemos@users.noreply.github.com>
2022-08-01 15:35:35 -0300

    Setup base project

---
+++ routes.js
@@ -0,0 +1,11 @@
+const Router = require("express");
+//const transactionRoutes = require("./src/modules/Routes/transactionRoutes")
+const routes = Router();
+
+
+routes.use("/users", (request,response)=>{});
+
+
+
+
+module.exports = routes;
